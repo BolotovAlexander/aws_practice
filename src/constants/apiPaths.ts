c6fa82a5bc6878bdf9f40586da561f12398b7066
Oleksandr Bolotov <oleksandr_bolotov@epam.com>
2023-10-16 10:00:07 +0300

    updated path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://eyh3lukhaa.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://eyh3lukhaa.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://eyh3lukhaa.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://eyh3lukhaa.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
