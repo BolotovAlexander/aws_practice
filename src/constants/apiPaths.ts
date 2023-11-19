476aa7f7e8730ec1733cf00a8eed72091155913e
Oleksandr Bolotov <oleksandr_bolotov@epam.com>
2023-11-19 16:54:44 +0200

    TASK_8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://4xodz2u761.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://t1lzd13yid.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://r30dl6yrr8.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
