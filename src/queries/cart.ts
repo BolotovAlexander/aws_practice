8ae93844fbb9f2962a60371fb9ecfed6fbde1395
Oleksandr Bolotov <oleksandr_bolotov@epam.com>
2023-11-19 20:50:13 +0200

    temp id

--- src/queries/cart.ts
+++ src/queries/cart.ts
@@ -6,7 +6,7 @@ import { CartItem } from "~/models/CartItem";
 
 export function useCart() {
   return useQuery<CartItem[], AxiosError>("cart", async () => {
-    const res = await axios.get<CartItem[]>(`${API_PATHS.cart}/profile/cart`, {
+    const res = await axios.get<CartItem[]>(`${API_PATHS.cart}/profile/cart/9117a11e-068d-49a9-82aa-69209b0088bd`, {
       headers: {
         Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
       },
